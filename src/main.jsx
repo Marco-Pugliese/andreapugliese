83961383186da7278288a9665fc9a1185a374226
Marco-Pugliese <marco.pugliese92@yahoo.com>
2025-02-10 14:10:19 +0100

    updd

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
-
 import store from "./Components/Redux/State/";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
