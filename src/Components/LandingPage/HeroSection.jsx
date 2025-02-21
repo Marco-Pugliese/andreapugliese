8acd012fbb805329181f7c268b259e5c527d10e8
Marco-Pugliese <marco.pugliese92@yahoo.com>
2025-02-21 10:41:22 +0100

    complete

--- src/Components/LandingPage/HeroSection.jsx
+++ src/Components/LandingPage/HeroSection.jsx
@@ -1,7 +1,4 @@
-import { useSelector } from "react-redux";
-
 const HeroSection = () => {
-  const LangInUse = useSelector((state) => state.Lang.lang);
   return (
     <div id="generalHero" className="p-0 position-relative filterSwap">
       <div className="h-100 filterblue">
@@ -10,49 +7,6 @@ const HeroSection = () => {
             <div className="phraseContainer">
               <span className="puff-in-center1 shadow-Light">
                 ANDREA PUGLIESE - CHEF
-              </span>
-              <span className="puff-in-center2 text-Italy">
-                {LangInUse === "Ita" && <>TRADIZIONE ITALIANA</>}
-                {LangInUse === "Esp" && <>TRADICIÓN ITALIANA</>}
-                {LangInUse === "Eng" && <>ITALIAN TRADITION</>}
-              </span>
-              <span className="puff-in-center3 text-Argentina">
-                {LangInUse === "Ita" && <>ANIMA ARGENTINA</>}
-                {LangInUse === "Esp" && <>ALMA ARGENTINA</>}
-                {LangInUse === "Eng" && <>ARGENTINE SOUL</>}
-              </span>
-              <span className="puff-in-center4 text-Spain">
-                {LangInUse === "Ita" && <>PASSIONE SPAGNOLA</>}
-                {LangInUse === "Esp" && <>PASIÓN ESPAÑOLA</>}
-                {LangInUse === "Eng" && <>SPANISH PASSION</>}
-              </span>
-
-              <span className="puff-in-center5 ">
-                <div className="d-flex flex-column justify-content-center align-items-around shadow-Light">
-                  <>
-                    {LangInUse === "Ita" && (
-                      <>
-                        <span className="mb-1 ">TRE RADICI.</span>
-
-                        <span className="mt-1">UN SOLO CUORE.</span>
-                      </>
-                    )}
-                    {LangInUse === "Esp" && (
-                      <>
-                        <span className="mb-1 ">TRES RAÍCES.</span>
-
-                        <span className="mt-1">UN SOLO CORAZÓN.</span>
-                      </>
-                    )}
-                    {LangInUse === "Eng" && (
-                      <>
-                        <span className="mb-1 ">THREE ROOTS.</span>
-
-                        <span className="mt-1">ONE HEART</span>
-                      </>
-                    )}
-                  </>
-                </div>
               </span>
             </div>
           </div>
